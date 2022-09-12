080089850d69c6eef04127cc6671b97443c7ebe7
18349244816 <601476706@qq.com>
2022-09-12 22:41:55 +0800

    update 2022.9.12

--- docs/assets/vue_proxy.md.54110b36.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as n,c as a,g as l}from"./app.bce87fe8.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"vue/proxy.md"}'),p={name:"vue/proxy.md"},o=l("",1),t=[o];function e(c,D,r,F,y,C){return n(),a("div",null,t)}var u=s(p,[["render",e]]);export{i as __pageData,u as default};
---
+++ docs/assets/vue_proxy.md.67a97072.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,d as l}from"./app.2df5cdfe.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"vue/proxy.md"}'),p={name:"vue/proxy.md"},o=l("",1),t=[o];function e(c,D,r,F,y,C){return n(),a("div",null,t)}var u=s(p,[["render",e]]);export{i as __pageData,u as default};
